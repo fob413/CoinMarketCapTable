adbf80936529982d477df960df9b0cc917bf1b8c
fob413 <oluwafunso.oluyole.balogun@gmail.com>
2024-01-05 15:03:56 -0400

    feat: pagination - creates pagination component - writes tests for pagination component

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,18 @@
-import React, { useMemo } from 'react';
+import React, { useMemo, useState } from 'react';
 import { Table } from "./components/table";
 import { useGetCryptoCurrency } from "./hooks/queries/useGetCryptoCurrency";
+import { Pagination } from "./components/pagination";
 import './App.css';
 
 function App() {
-  const { data } = useGetCryptoCurrency();
+  const [total, setTotal] = useState(0);
+  const [page, setPage] = useState(1);
+  const { data } = useGetCryptoCurrency(page);
 
   const cryptoCurrencyData = useMemo(() => {
     if (!data && !data?.hasOwnProperty('data')) return [];
+
+    setTotal(data?.status?.total_count);
 
     return data?.data?.map((row: any | unknown) => {
       return {
@@ -25,6 +30,7 @@ function App() {
   return (
     <div className="App" data-testid='App'>
         <Table data={cryptoCurrencyData} />
+        <Pagination total={total} onChange={setPage} />
     </div>
   );
 }
--- src/hooks/queries/useGetCryptoCurrency.tsx
+++ src/hooks/queries/useGetCryptoCurrency.tsx
@@ -3,14 +3,15 @@ import { MOCKEDCRYPTOCURRENCY } from './mocks/cyptoCurrency';
 
 
 
-const getCryptoCurrency = (): any => {
+const getCryptoCurrency = (page: number): any => {
     // returns a mocked api response data
+    // console.log('>>> page', page * 10);
     return MOCKEDCRYPTOCURRENCY;
 }
 
-export const useGetCryptoCurrency = () => {
+export const useGetCryptoCurrency = (page: number) => {
     return useQuery({
-        queryKey: ['crypto-currency'],
-        queryFn: getCryptoCurrency
+        queryKey: [`crypto-currency-${page}`],
+        queryFn: () => getCryptoCurrency(page)
     })
 }
