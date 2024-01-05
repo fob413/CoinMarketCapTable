3934ff221328a519f0c01a93bb8ee1927a7c3e9f
fob413 <oluwafunso.oluyole.balogun@gmail.com>
2024-01-05 13:41:47 -0400

    feat: components - implements table component - implements tests for table component - updated app tests

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import logo from './logo.svg';
+import { Table } from "./components/table";
 import { useGetCryptoCurrency } from "./hooks/queries/useGetCryptoCurrency";
 import './App.css';
 
@@ -8,21 +8,8 @@ function App() {
   console.log('>>>> ', data);
 
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App" data-testid='App'>
+        <Table />
     </div>
   );
 }
